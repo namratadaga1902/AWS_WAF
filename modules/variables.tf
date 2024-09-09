837d4f8e81639f936dd4ea235441fb76574d3df4
namratadaga1902 <namratadaga1902@gmail.com>
2024-09-09 23:06:30 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,47 +1,45 @@
 variable "aws_region" {
-  description = "The AWS region where resources will be created."
+  description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
 }
 
 variable "web_acl_name" {
-  description = "The name of the WAF Web ACL."
+  description = "Name of the WAFv2 Web ACL"
   type        = string
-  default     = "example-web-acl"
-}
-
-variable "web_acl_description" {
-  description = "Description of the WAF Web ACL."
-  type        = string
-  default     = "Example WAF ACL with logging and CloudWatch metrics"
 }
 
 variable "web_acl_scope" {
-  description = "Scope of the WAF Web ACL (REGIONAL or CLOUDFRONT)."
+  description = "Scope of the WAFv2 Web ACL (REGIONAL or CLOUDFRONT)"
   type        = string
   default     = "REGIONAL"
 }
 
+variable "web_acl_description" {
+  description = "Description of the WAFv2 Web ACL"
+  type        = string
+  default     = "AWS WAFv2 Web ACL"
+}
+
 variable "metric_name" {
-  description = "CloudWatch metric name for WAF Web ACL."
+  description = "CloudWatch metric name for the WAFv2 Web ACL"
   type        = string
-  default     = "waf-metrics"
 }
 
-variable "rate_limit" {
-  description = "The rate limit for the rate-based rule."
-  type        = number
-  default     = 1000
-}
-
-variable "log_destination_configs" {
-  description = "List of ARNs for logging destinations (e.g., S3, CloudWatch Logs)."
-  type        = list(string)
-  default     = []
-}
-
-variable "global_web_acl_name" {
-  description = "The name of the WAF Classic Global Web ACL."
+variable "log_group_name" {
+  description = "Name of the CloudWatch Log Group for WAF logs"
   type        = string
-  default     = "example-global-web-acl"
+  default     = "/aws/waf/logs"
+}
+
+variable "rules" {
+  description = "List of rules for the WAFv2 Web ACL"
+  type = list(object({
+    name              = string
+    priority          = number
+    metric_name       = string
+    rate_limit        = number
+    aggregate_key_type = string
+  }))
+  default = []
 }
