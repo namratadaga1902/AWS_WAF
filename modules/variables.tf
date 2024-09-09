05a12eb741873c41bd34cb60f0145034b80d3f72
namratadaga1902 <namratadaga1902@gmail.com>
2024-09-09 23:07:00 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,45 +1,9 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
+output "wafv2_web_acl_arn" {
+  description = "The ARN of the WAFv2 Web ACL"
+  value       = aws_wafv2_web_acl.this.arn
 }
 
-variable "web_acl_name" {
-  description = "Name of the WAFv2 Web ACL"
-  type        = string
-}
-
-variable "web_acl_scope" {
-  description = "Scope of the WAFv2 Web ACL (REGIONAL or CLOUDFRONT)"
-  type        = string
-  default     = "REGIONAL"
-}
-
-variable "web_acl_description" {
-  description = "Description of the WAFv2 Web ACL"
-  type        = string
-  default     = "AWS WAFv2 Web ACL"
-}
-
-variable "metric_name" {
-  description = "CloudWatch metric name for the WAFv2 Web ACL"
-  type        = string
-}
-
-variable "log_group_name" {
-  description = "Name of the CloudWatch Log Group for WAF logs"
-  type        = string
-  default     = "/aws/waf/logs"
-}
-
-variable "rules" {
-  description = "List of rules for the WAFv2 Web ACL"
-  type = list(object({
-    name              = string
-    priority          = number
-    metric_name       = string
-    rate_limit        = number
-    aggregate_key_type = string
-  }))
-  default = []
+output "cloudwatch_log_group_arn" {
+  description = "The ARN of the CloudWatch Log Group for WAF logging"
+  value       = aws_cloudwatch_log_group.waf_log_group.arn
 }
