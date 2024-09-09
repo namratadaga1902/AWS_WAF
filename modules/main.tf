2488913ce58f39d84599f22dc8259289f686b81d
namratadaga1902 <namratadaga1902@gmail.com>
2024-09-09 23:05:41 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.aws_region
 }
 
-# WAFv2 Web ACL creation
-resource "aws_wafv2_web_acl" "example" {
+# AWS WAFv2 Web ACL
+resource "aws_wafv2_web_acl" "this" {
   name        = var.web_acl_name
   scope       = var.web_acl_scope
   description = var.web_acl_description
@@ -18,54 +18,34 @@ resource "aws_wafv2_web_acl" "example" {
     sampled_requests_enabled   = true
   }
 
-  rule {
-    name     = "rate-limit-rule"
-    priority = 1
+  dynamic "rule" {
+    for_each = var.rules
+    content {
+      name     = rule.value.name
+      priority = rule.value.priority
 
-    statement {
-      rate_based_statement {
-        aggregate_key_type = "IP"
-        limit              = var.rate_limit
+      action {
+        block {}
+      }
+
+      statement {
+        # Replace this with the appropriate statement type from the input
+        rate_based_statement {
+          limit              = rule.value.rate_limit
+          aggregate_key_type = rule.value.aggregate_key_type
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = rule.value.metric_name
+        sampled_requests_enabled   = true
       }
     }
-
-    action {
-      block {}
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "rate-limit"
-      sampled_requests_enabled   = true
-    }
   }
 }
 
-# Enable WAFv2 Web ACL Logging
-resource "aws_wafv2_web_acl_logging_configuration" "example_logging" {
-  resource_arn            = aws_wafv2_web_acl.example.arn
-  log_destination_configs = var.log_destination_configs
-}
-
-# WAF Classic Global Web ACL Logging (AWS WAF Classic)
-resource "aws_waf_web_acl" "global_classic" {
-  name        = var.global_web_acl_name
-  metric_name = "globalWafMetrics"
-  default_action {
-    type = "ALLOW"
-  }
-
-  rules {
-    action {
-      type = "BLOCK"
-    }
-    priority = 1
-    rule_id  = aws_waf_rule.example.id
-  }
-}
-
-# Example rule for WAF Classic Global ACL
-resource "aws_waf_rule" "example" {
-  name        = "example-rule"
-  metric_name = "examplemetrics"
+# CloudWatch Log Group for WAF Logging
+resource "aws_cloudwatch_log_group" "waf_log_group" {
+  name = var.log_group_name
 }
