3e4e5a26c54acaee49ad2f952f8ef32199668aae
namratadaga1902 <namratadaga1902@gmail.com>
2024-09-09 23:21:38 +0530

    Update outputs.tf

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,9 +1,9 @@
-output "web_acl_id" {
-  description = "The ID of the WAF Web ACL."
-  value       = aws_wafv2_web_acl.example.id
+output "wafv2_web_acl_arn" {
+  description = "The ARN of the WAFv2 Web ACL"
+  value       = aws_wafv2_web_acl.this.arn
 }
 
-output "global_classic_web_acl_id" {
-  description = "The ID of the WAF Classic Global Web ACL."
-  value       = aws_waf_web_acl.global_classic.id
+output "cloudwatch_log_group_arn" {
+  description = "The ARN of the CloudWatch Log Group for WAF logging"
+  value       = aws_cloudwatch_log_group.waf_log_group.arn
 }
